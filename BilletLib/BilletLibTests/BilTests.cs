dbd967fd9629a62fe012b4f653c6e364f15fadf6
Lasse Andersen <LasseAndersen@users.noreply.github.com>
2018-01-30 14:46:30 +0100

    opgave 1, 2 og 3 lavet.

---
+++ BilletLib/BilletLibTests/BilTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BilletLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilletLib.Tests
+{
+    [TestClass()]
+    public class BilTests
+    {
+        [TestMethod()]
+        public void PrisTest()
+        {
+            //Arrange
+
+            var PrisPåBil = new Bil();
+
+            //Act
+
+            int pris = PrisPåBil.Pris();
+
+
+            Assert.AreEqual(240, pris);
+        }
+
+        [TestMethod()]
+        public void køretøjTest()
+        {
+            var køretøjsType = new Bil();
+
+            var type = køretøjsType.køretøj();
+
+
+            Assert.AreEqual("Bil", type);
+        }
+    }
+}
---
+++ BilletLib/BilletLibTests/MCTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BilletLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BilletLib.Tests
+{
+    [TestClass()]
+    public class MCTests
+    {
+        [TestMethod()]
+        public void PrisTest()
+        {
+
+            //Arrange
+
+            var PrisPåMC = new MC();
+
+            //Act
+
+            int pris = PrisPåMC.Pris();
+
+
+            Assert.AreEqual(125, pris);
+        }
+
+        [TestMethod()]
+        public void KøretøjTest()
+        {
+            var køretøjsType = new MC();
+
+            var type = køretøjsType.Køretøj();
+
+
+            Assert.AreEqual("MC", type);
+        }
+    }
+}
